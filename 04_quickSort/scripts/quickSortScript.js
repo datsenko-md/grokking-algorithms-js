e80a84bf98d786285a4ae59639f2d726cdf01c6c
Max Datsenko <datsenko.md@gmail.com>
2017-10-05 13:11:20 +0300

    add chapter: 04_quickSort

---
+++ 04_quickSort/scripts/quickSortScript.js
@@ -0,0 +1,6 @@
+import quickSort from '../source/quickSort';
+import randomSequence from '../../00_other/randomSequence';
+
+const array = randomSequence(1, 10);
+console.log("Sequence: " + array);
+console.log("Sorted: " + quickSort(array));
